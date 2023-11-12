664fec479080e64689f4d0fd1748f8e4064f8da9
Marco Aspeitia <maspeitia.31@gmail.com>
2023-11-12 00:18:09 -0600

    404 page

--- template-parts/content-front.php
+++ template-parts/content-front.php
@@ -62,45 +62,32 @@
 		<div class="row">
 			<div class="col-xl-6 offset-xl-3 col-lg-8 offset-lg-2">
 				<div class="section-title wow fadeInUp" data-wow-delay="0.3s" data-wow-duration="1s">
-					<h2>apps features</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod te incididunt ut labore et dolore magna aliqua. Ut enim ad minim to eismud </p>
+					<h2><?php echo esc_html( get_post_meta( get_the_id(), 'front_page_features_title', true ) ); ?></h2>
+					<p><?php echo esc_html( get_post_meta( get_the_id(), 'front_page_features_content', true ) ); ?></p>
 				</div>
 			</div>
 			<div class="col-lg-12">
 				<div class="row justify-content-center">
-					<div class="col-lg-4 col-md-6">
-						<div class="item wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1s">
-							<div class="icon">
-								<i class="flaticon-setup"></i>
+					<?php
+					$features = get_post_meta( get_the_ID(), 'front_page_features_features', true );
+
+					if( ! empty( $features ) ):
+
+						foreach( $features as $feature ):
+						?>
+						<div class="col-lg-4 col-md-6">
+							<div class="item wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1s">
+								<div class="icon">
+									<i class="flaticon-setup"></i>
+								</div>
+								<h5><?php echo esc_html( $feature[ 'title' ] )?></h5>
+								<p><?php echo esc_html( $feature[ 'content' ] )?></p>
 							</div>
-							<span>01</span>
-							<h5>social marketing</h5>
-							<p>Lorem ipsum dolor sit amet, co adipisicing elit, sed do eiusmod tempor incididunt uty labore et dolore magna aliqua. Ut enimd minim veniam, quis nostrud</p>
-							<a href="service-detail.html"><i class="flaticon-arrow-pointing-to-right"></i></a>
 						</div>
-					</div>
-					<div class="col-lg-4 col-md-6">
-						<div class="item wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1s">
-							<div class="icon">
-								<i class="flaticon-mobile-app-developing"></i>
-							</div>
-							<span>02</span>
-							<h5>prepare daily report</h5>
-							<p>Lorem ipsum dolor sit amet, co adipisicing elit, sed do eiusmod tempor incididunt uty labore et dolore magna aliqua. Ut enimd minim veniam, quis nostrud</p>
-							<a href="service-detail.html"><i class="flaticon-arrow-pointing-to-right"></i></a>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-6">
-						<div class="item wow fadeInUp" data-wow-delay="0.6s" data-wow-duration="1s">
-							<div class="icon">
-								<i class="flaticon-technology"></i>
-							</div>
-							<span>03</span>
-							<h5>analysis your data</h5>
-							<p>Lorem ipsum dolor sit amet, co adipisicing elit, sed do eiusmod tempor incididunt uty labore et dolore magna aliqua. Ut enimd minim veniam, quis nostrud</p>
-							<a href="service-detail.html"><i class="flaticon-arrow-pointing-to-right"></i></a>
-						</div>
-					</div>
+						<?php
+						endforeach;
+					endif;
+					?>
 				</div>
 			</div>
 		</div>
