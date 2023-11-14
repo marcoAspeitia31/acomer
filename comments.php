9d25f2dad79766318a2e8a0e506611b20d4a1244
Marco Aspeitia <maspeitia.31@gmail.com>
2023-11-13 19:39:19 -0600

    improoving single.php css

--- comments.php
+++ comments.php
@@ -19,63 +19,60 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
-<div class="container">
-	<div class="row">
-		<div class="col-lg-12">
-			<div id="comments" class="comments-area">
+
+<div class="col-lg-12">
+	<div id="comments" class="comments-area comment">
+		<?php
+		// You can start editing here -- including this comment!
+		if ( have_comments() ) :
+			?>
+			<h4 class="comments-title">
 				<?php
-				// You can start editing here -- including this comment!
-				if ( have_comments() ) :
-					?>
-					<h2 class="comments-title">
-						<?php
-						$acomer_comment_count = get_comments_number();
-						if ( '1' === $acomer_comment_count ) {
-							printf(
-								/* translators: 1: title. */
-								esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'acomer' ),
-								'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-							);
-						} else {
-							printf( 
-								/* translators: 1: comment count number, 2: title. */
-								esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $acomer_comment_count, 'comments title', 'acomer' ) ),
-								number_format_i18n( $acomer_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-								'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-							);
-						}
-						?>
-					</h2><!-- .comments-title -->
-
-					<?php the_comments_navigation(); ?>
-
-					<ol class="comment-list">
-						<?php
-						wp_list_comments(
-							array(
-								'style'      => 'ol',
-								'short_ping' => true,
-							)
-						);
-						?>
-					</ol><!-- .comment-list -->
-
-					<?php
-					the_comments_navigation();
-
-					// If comments are closed and there are comments, let's leave a little note, shall we?
-					if ( ! comments_open() ) :
-						?>
-						<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'acomer' ); ?></p>
-						<?php
-					endif;
-
-				endif; // Check for have_comments().
-
-				comment_form();
+				$acomer_comment_count = get_comments_number();
+				if ( '1' === $acomer_comment_count ) {
+					printf(
+						/* translators: 1: title. */
+						esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'acomer' ),
+						'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+					);
+				} else {
+					printf( 
+						/* translators: 1: comment count number, 2: title. */
+						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $acomer_comment_count, 'comments title', 'acomer' ) ),
+						number_format_i18n( $acomer_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+					);
+				}
 				?>
-			</div><!-- #comments -->
-		</div>
-	</div>
-</div>
+			</h4><!-- .comments-title -->
+			
 
+			<?php the_comments_navigation(); ?>
+
+			<ol class="comment-list all-comment">
+				<?php
+				wp_list_comments(
+					array(
+						'style'      => 'ol',
+						'short_ping' => true,
+					)
+				);
+				?>
+			</ol><!-- .comment-list -->
+
+			<?php
+			the_comments_navigation();
+
+			// If comments are closed and there are comments, let's leave a little note, shall we?
+			if ( ! comments_open() ) :
+				?>
+				<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'acomer' ); ?></p>
+				<?php
+			endif;
+
+		endif; // Check for have_comments().
+
+		comment_form();
+		?>
+	</div><!-- #comments -->
+</div>
